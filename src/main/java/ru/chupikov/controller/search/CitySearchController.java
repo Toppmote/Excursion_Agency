78053fd20a3290186476c668de39fb17b8e0a803
Pavel Chupikov <toppmote@gmail.com>
2022-02-08 01:26:31 +0700

    Сделал метод для взятия всех городов из БД

--- src/main/java/ru/chupikov/controller/search/CitySearchController.java
+++ src/main/java/ru/chupikov/controller/search/CitySearchController.java
@@ -1,16 +1,29 @@
 package ru.chupikov.controller.search;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.chupikov.entity.CityEntity;
 import ru.chupikov.service.search.CitySearchService;
+
+import java.util.List;
 
 /**
  * Контроллер для операций поиска, связанных с городами
  */
-@RestController
+@Controller
 public class CitySearchController {
 
     @Autowired
     CitySearchService citySearchService;
+
+    @GetMapping("/cities")
+    public String loadCitiesPage(Model model) {
+        List<CityEntity> cities = citySearchService.findAll();
+        model.addAttribute("cities", cities);
+        return "cities";
+    }
+
 
 }
--- src/main/java/ru/chupikov/service/search/CitySearchService.java
+++ src/main/java/ru/chupikov/service/search/CitySearchService.java
@@ -1,8 +1,12 @@
 package ru.chupikov.service.search;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import ru.chupikov.dao.CityRepository;
+import ru.chupikov.entity.CityEntity;
+
+import java.util.List;
 
 /**
  * Сервис с операциями поиска для городов
@@ -12,5 +16,10 @@ public class CitySearchService {
 
     @Autowired
     CityRepository cityRepository;
+
+    public List<CityEntity> findAll(){
+        return cityRepository.findAll(Sort.by("name"));
+    }
+
 
 }

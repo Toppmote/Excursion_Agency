fee8844e77bf1eb939455b4234f23d5a65b640e9
Pavel Chupikov <toppmote@gmail.com>
2022-02-13 20:40:53 +0700

    Добавил аннотацию @EntityScan

--- src/main/java/ru/chupikov/Application.java
+++ src/main/java/ru/chupikov/Application.java
@@ -2,8 +2,10 @@ package ru.chupikov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("ru.chupikov.entity")
 public class Application {
 
     public static void main(String[] args) {
